2aa5d5a4f5940d7f0b9e46318723c4fc387a5656
Joanna Lin <linjoanna31@gmail.com>
2022-02-16 15:06:17 +1100

    Added week 1 content

---
+++ tut01/tut01/face0.c
@@ -0,0 +1,18 @@
+// This is a program that prints a face
+// W13C, February 2022
+
+#include <stdio.h>
+
+int main(void) {
+    // Prints the face:
+    // ~ ~
+    // 0 0
+    //  o
+    //  -
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+    
+    return 0;
+}
---
+++ tut01/tut01/face1.c
@@ -0,0 +1,18 @@
+// This is a program that prints a happy face
+// W13C, February 2022
+
+#include <stdio.h>
+
+int main(void) {
+    // Prints this face:
+    // ~ ~
+    // 0 0
+    //  o
+    // \_/
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf("\\_/\n");
+    
+    return 0;
+}
